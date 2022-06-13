5802166bc5885525a9384274c5b6fa1a0904a4e1
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2022-06-13 12:37:45 +0300

    Add project files.

---
+++ Wikipedia.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+//Create new Chome brose instanse
+var driver = new ChromeDriver();
+
+//Navigate to Wikipedia
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE:" + driver.Title);
+
+//locate search field by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+//Click on the selected element
+searchField.Click();
+
+//fill QA
+searchField.SendKeys("QA");
+
+// press ENTER beyboard
+searchField.SendKeys(Keys.Enter);
+
+System.Console.WriteLine("TITLE AFTER SEARCH:" + driver.Title);
+
+//Close broser
+driver.Quit();
+
+
